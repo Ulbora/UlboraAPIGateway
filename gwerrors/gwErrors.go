ae75f2e8c02fabff0d971ab457750432e9ed7c7c
ken <kenulborapublic@gmail.com>
2017-11-14 22:03:23 -0500

    updates

--- gwerrors/gwErrors.go
+++ gwerrors/gwErrors.go
@@ -25,8 +25,15 @@
 package gwerrors
 
 import (
+	db "UlboraApiGateway/database"
+	"fmt"
 	"time"
 )
+
+//GatewayErrorMonitor error monitor
+type GatewayErrorMonitor struct {
+	DbConfig db.DbConfig
+}
 
 //GwError GwError
 type GwError struct {
@@ -37,4 +44,44 @@ type GwError struct {
 	RouteURIID  int64
 	RestRouteID int64
 	ClientID    int64
+}
+
+//ConnectDb to database
+func (g *GatewayErrorMonitor) ConnectDb() bool {
+	rtn := g.DbConfig.ConnectDb()
+	if rtn == true {
+		fmt.Println("db connect")
+	}
+	return rtn
+}
+
+//InsertRouteError insert
+func (g *GatewayErrorMonitor) InsertRouteError(e *GwError) (bool, error) {
+	var success bool
+	var err error
+	dbConnected := g.DbConfig.ConnectionTest()
+	if !dbConnected {
+		fmt.Println("reconnection to closed database")
+		g.DbConfig.ConnectDb()
+	}
+	//var a []interface{}
+	a := []interface{}{e.Code, e.Message, time.Now().Add(time.Hour * -2400), e.RouteURIID, e.RestRouteID, e.ClientID}
+	suc, insID := g.DbConfig.InsertRouteError(a...)
+	if suc == true && insID != -1 {
+		success = suc
+		//fmt.Print("new Id route error id: ")
+		//fmt.Println(insID)
+	} else {
+		err = fmt.Errorf("Failed to insert Error Record")
+	}
+	return success, err
+}
+
+//CloseDb connection to database
+func (g *GatewayErrorMonitor) CloseDb() bool {
+	rtn := g.DbConfig.CloseDb()
+	if rtn == true {
+		fmt.Println("db connect closed")
+	}
+	return rtn
 }
---
+++ gwerrors/gwErrors_test.go
@@ -0,0 +1,109 @@
+package gwerrors
+
+import (
+	mgr "UlboraApiGateway/managers"
+	"fmt"
+	"testing"
+)
+
+var gatewayDB GatewayErrorMonitor
+var gatewayDB2 mgr.GatewayDB
+var connected1 bool
+var connected2 bool
+var clientID int64
+var insertID int64
+
+var routeID int64
+
+var routeURLID int64
+
+func TestGatewayErrorMonitor_ConnectDb(t *testing.T) {
+	clientID = 4
+	gatewayDB.DbConfig.Host = "localhost:3306"
+	gatewayDB.DbConfig.DbUser = "admin"
+	gatewayDB.DbConfig.DbPw = "admin"
+	gatewayDB.DbConfig.DatabaseName = "ulbora_api_gateway"
+	connected1 = gatewayDB.ConnectDb()
+
+	gatewayDB2.DbConfig.Host = "localhost:3306"
+	gatewayDB2.DbConfig.DbUser = "admin"
+	gatewayDB2.DbConfig.DbPw = "admin"
+	gatewayDB2.DbConfig.DatabaseName = "ulbora_api_gateway"
+	connected2 = gatewayDB2.ConnectDb()
+	if connected1 != true || connected2 != true {
+		t.Fail()
+	}
+}
+
+func TestGatewayErrorMonitor_InsertClient(t *testing.T) {
+	var c mgr.Client
+	c.APIKey = "12233hgdd333"
+	c.ClientID = clientID
+	c.Enabled = true
+	c.Level = "small"
+
+	res := gatewayDB2.InsertClient(&c)
+	if res.Success == true && res.ID != -1 {
+		fmt.Print("new client Id: ")
+		fmt.Println(clientID)
+	} else {
+		fmt.Println("database insert failed")
+		t.Fail()
+	}
+}
+
+func TestGatewayErrorMonitor_InsertRestRoute(t *testing.T) {
+	var rr mgr.RestRoute
+	rr.Route = "content"
+	rr.ClientID = clientID
+
+	res := gatewayDB2.InsertRestRoute(&rr)
+	if res.Success == true && res.ID != -1 {
+		routeID = res.ID
+		fmt.Print("new route Id: ")
+		fmt.Println(routeID)
+	} else {
+		fmt.Println("database insert failed")
+		t.Fail()
+	}
+}
+
+func TestGatewayErrorMonitor_InsertRouteURL(t *testing.T) {
+	var ru mgr.RouteURL
+	ru.Name = "blue"
+	ru.URL = "http://www.apigateway.com/blue/"
+	ru.Active = false
+	ru.RouteID = routeID
+	ru.ClientID = clientID
+
+	res := gatewayDB2.InsertRouteURL(&ru)
+	if res.Success == true && res.ID != -1 {
+		routeURLID = res.ID
+		fmt.Print("new route url Id: ")
+		fmt.Println(routeURLID)
+	} else {
+		fmt.Println("database insert failed")
+		t.Fail()
+	}
+}
+
+func TestGatewayErrorMonitor_InsertRouteError(t *testing.T) {
+	var e GwError
+	e.ClientID = clientID
+	e.Code = 500
+	e.Message = "internal error"
+	e.RestRouteID = routeID
+	e.RouteURIID = routeURLID
+	suc, err := gatewayDB.InsertRouteError(&e)
+	if suc != true || err != nil {
+		t.Fail()
+	}
+}
+
+func TestGatewayErrorMonitor_TestCloseDb(t *testing.T) {
+	success := gatewayDB.CloseDb()
+	success2 := gatewayDB2.CloseDb()
+	if success != true || success2 != true {
+		t.Fail()
+	}
+}
